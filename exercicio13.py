2d3803080d9a08af4ccc09284620671b47225138
Luiz Henrique <luizhenriqueprudenciosouza@gmail.com>
2021-09-02 13:50:24 -0300

    Create exercicio13.py

---
+++ exercicio13.py
@@ -0,0 +1,5 @@
+k = float(input("Digite uma distância em quilometros: "))
+
+m = k / 1.61
+
+print("A distância digitada é de {} quilometros, essa distância convertida é {:.2f} milhas" .format(k,m))
