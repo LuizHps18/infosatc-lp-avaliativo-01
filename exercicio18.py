cccaf1ec71213623920a2d8d67777cd1af6e5564
Luiz Henrique <luizhenriqueprudenciosouza@gmail.com>
2021-09-02 13:51:56 -0300

    Create exercicio18.py

---
+++ exercicio18.py
@@ -0,0 +1,5 @@
+vm = float(input("Digite um volume em metros cubicos: "))
+
+lc = 1000 * vm
+
+print("O volume digitado é de {} metros cubicos, esse valor convertido é em volume {:.2f} litros " .format(vm,lc))
