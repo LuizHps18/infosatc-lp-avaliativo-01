3b5df847c82789380de5d303de90d5855c4cfc40
Luiz Henrique <luizhenriqueprudenciosouza@gmail.com>
2021-09-02 13:57:00 -0300

    exercicio 24
    :d

---
+++ exercicio24.py
@@ -0,0 +1,5 @@
+mq = float(input("Digite um valor de area em metros quadrados: "))
+
+a = mq * 0.000247
+
+print("O valor da area digitada é de {} metros quadrados, esse valor convertido em {} acres" .format(mq,a))
