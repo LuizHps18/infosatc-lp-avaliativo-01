018104e57529c2d41db27f11a101cef9f1aad521
Luiz Henrique <luizhenriqueprudenciosouza@gmail.com>
2021-09-02 13:58:39 -0300

    exercicio 26
    :v

---
+++ exercicio26.py
@@ -0,0 +1,5 @@
+m = float(input("Digite um valor de uma area em metros quadrados: "))
+
+h = m * 0.0001
+
+print("O valor digitado é de {} metros quadrados, esse valor convertido é de {} hectares" .format(m,h))
